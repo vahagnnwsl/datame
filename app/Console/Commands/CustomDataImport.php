14b2b4205bdeb2d5b0a14eaf19339f742074719d
anwsonwsymous <anwsonwsymous11@gmail.com>
2020-06-24 16:59:06 +0400

    Added memory usage check

--- app/Console/Commands/CustomDataImport.php
+++ app/Console/Commands/CustomDataImport.php
@@ -40,7 +40,7 @@ class CustomDataImport extends Command
             $i = 0;
             $iterator = $this->generateData($customDataImport);
             foreach ($iterator as $datum) {
-                CustomData::query()->insert($datum);
+                CustomData::query()->updateOrInsert(array_keys($datum), $datum);
                 $i++;
                 if ($i % 500 == 0) {
                     $this->info(round(memory_get_usage() / 1024 / 1024, 2) . ' MB');
